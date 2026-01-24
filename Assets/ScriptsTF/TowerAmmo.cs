90b4fd522b924abe519d34f6e5a4752251e13ac2
Chavadex <saesccm1905@gmail.com>
2026-01-24 16:47:57 -0600

    Torret Minor Changes

--- Assets/ScriptsTF/TowerAmmo.cs
+++ Assets/ScriptsTF/TowerAmmo.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TowerAmmo : MonoBehaviour
 {
@@ -10,39 +11,31 @@ public class TowerAmmo : MonoBehaviour
         Green
     }
 
-
     [Header("Ammo Settings")]
     [SerializeField] private AmmoType currentAmmoType;
     [SerializeField] private int maxAmmo = 20;
     [SerializeField] private int currentAmmo;
 
-    [Header("Ammo UI")]
+    [Header("Ammo UI - Text")]
     [SerializeField] private TextMeshProUGUI currentAmmoText;
+
+    [Header("Ammo UI - Images")]
+    [SerializeField] private Image imageAmmoRed;
+    [SerializeField] private Image imageAmmoBlue;
+    [SerializeField] private Image imageAmmoGreen;
 
     void Awake()
     {
         currentAmmo = maxAmmo;
-        currentAmmoText.text = currentAmmo + "/" + maxAmmo;
+        UpdateAmmoText();
+        UpdateAmmoImages();
     }
-
-    
 
     // ================= GETTERS =================
 
-    public AmmoType GetAmmoType()
-    {
-        return currentAmmoType;
-    }
-
-    public int GetCurrentAmmo()
-    {
-        return currentAmmo;
-    }
-
-    public bool HasAmmo()
-    {
-        return currentAmmo > 0;
-    }
+    public AmmoType GetAmmoType() => currentAmmoType;
+    public int GetCurrentAmmo() => currentAmmo;
+    public bool HasAmmo() => currentAmmo > 0;
 
     // ================= USO =================
 
@@ -50,8 +43,7 @@ public class TowerAmmo : MonoBehaviour
     {
         currentAmmo -= amount;
         currentAmmo = Mathf.Clamp(currentAmmo, 0, maxAmmo);
-        currentAmmoText.text = currentAmmo + "/" + maxAmmo;
-
+        UpdateAmmoText();
     }
 
     // ================= PLAYER ACTIONS =================
@@ -60,11 +52,46 @@ public class TowerAmmo : MonoBehaviour
     {
         Debug.Log("Ya recargada");
         currentAmmo = maxAmmo;
+        UpdateAmmoText();
     }
 
     public void ChangeAmmoType(AmmoType newType)
     {
         currentAmmoType = newType;
+        UpdateAmmoImages();
+    }
+
+    // ================= UI =================
+
+    private void UpdateAmmoText()
+    {
+        if (currentAmmoText != null)
+        {
+            currentAmmoText.text = $"{currentAmmo}/{maxAmmo}";
+        }
+    }
+
+    private void UpdateAmmoImages()
+    {
+        if (imageAmmoRed != null) imageAmmoRed.enabled = false;
+        if (imageAmmoBlue != null) imageAmmoBlue.enabled = false;
+        if (imageAmmoGreen != null) imageAmmoGreen.enabled = false;
+
+        switch (currentAmmoType)
+        {
+            case AmmoType.Red:
+                if (imageAmmoRed != null) imageAmmoRed.enabled = true;
+                break;
+
+            case AmmoType.Blue:
+                if (imageAmmoBlue != null) imageAmmoBlue.enabled = true;
+                break;
+
+            case AmmoType.Green:
+                if (imageAmmoGreen != null) imageAmmoGreen.enabled = true;
+                break;
+        }
     }
 }
+
 
