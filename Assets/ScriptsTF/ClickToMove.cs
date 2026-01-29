1a3d52fccb8ddd94a91025df454e444407fd3eaa
ErikPrograa <erikdevel07@gmail.com>
2026-01-29 04:05:10 -0800

    feat: implemented player model and added animation to player and enemies, improved ui design a bit i think

--- Assets/ScriptsTF/ClickToMove.cs
+++ Assets/ScriptsTF/ClickToMove.cs
@@ -16,10 +16,15 @@ public class ClickToMove : MonoBehaviour
     private NavMeshAgent agent;
 
     private Camera mainCamera;
+    private Animator anim;
     void Start()
 
     {
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponentInChildren<Animator>(true);
+
+        if (anim == null)
+        Debug.LogError("No encontré Animator en hijos");
 
         mainCamera = Camera.main;
 
@@ -40,7 +45,7 @@ public class ClickToMove : MonoBehaviour
     {
         agent.speed = moveSpeed;
 
-        if (Input.GetMouseButtonDown(0))
+                if (Input.GetMouseButtonDown(0))
 
         {
             if (EventSystem.current != null && EventSystem.current.IsPointerOverGameObject())
@@ -49,6 +54,13 @@ public class ClickToMove : MonoBehaviour
             MoveToClickPosition();
 
         }
+
+        if (anim != null)
+        {
+            float speed = agent.velocity.magnitude; // 0 = idle, >0 = running
+            anim.SetFloat("Speed", speed);
+        }
+
 
     }
 
